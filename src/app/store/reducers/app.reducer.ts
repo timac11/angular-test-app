f5265472df600fa250466ffe1b5cf486e67a751a
aleksandrsokolov <timac11@bk.ru>
2020-04-21 23:59:01 +0300

    Auto-generated commit

--- src/app/store/reducers/app.reducer.ts
+++
@@ -1,9 +0,0 @@
-import {ActionReducerMap} from '@ngrx/store';
-import {AppState} from '../state/app.state';
-import {routerReducer} from '@ngrx/router-store';
-import {eventsReducer} from './events.reducer';
-
-export const appReducers: ActionReducerMap<AppState, any> = {
-  router: routerReducer,
-  events: eventsReducer
-};
